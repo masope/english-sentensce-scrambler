47a6cab43408bcae89ad83bca258bccfdc609578
H. Do-Young <hwjds0529@gmail.com>
2021-03-23 00:39:04 +0900

    String Input Stage

--- mainProg.go
+++ mainProg.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"bufio"
+	"os"
+)
+
+func str_stdInput(valMem *string){
+	sc := bufio.NewScanner(os.Stdin)
+	sc.Scan()
+	*valMem = sc.Text()
+	return
+}
 
 func main(){
-	fmt.Println("English Sentence Scrambler")
+	var TargetSentence string
+	var TranslatedSentence string
+	
+	fmt.Print("영어 문장을 입력해 주세요 >> ")
+	str_stdInput(&TargetSentence)
+
+	fmt.Print("한글 문장을 입력해 주세요 >> ")
+	str_stdInput(&TranslatedSentence)
+
+
+	fmt.Printf("%s and %s\n", TargetSentence, TranslatedSentence)
 }
+
